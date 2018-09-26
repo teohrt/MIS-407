56721fc424f4cfab1612761752b52f04f073d662
Trace Ohrt <teohrt18@gmail.com>
2018-09-26 13:52:14 -0500

    Fixed module name bug and added doc strings

--- IA02/ia02/logging.py
+++ IA02/ia02/logging.py
@@ -7,9 +7,15 @@ import os
 import sys
 from datetime import datetime
 
+"""
+Only allows the module to be used when it's imported
+"""
 if (__name__ == '__main__'):
     print("Sorry, not for human consumption.")
     quit()
 
+"""
+Logs datetime, function's module, name and message
+"""
 def log_function(f,  message):
-    print('{} {} {}: {}'.format(datetime.now().isoformat(sep=' '), dir(f), f.__name__, message))
+    print('{} {} {}: {}'.format(datetime.now().isoformat(sep=' '), f.__module__, f.__name__, message))
