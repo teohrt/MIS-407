fd5b3260c7f44d434d81aa3ed292274a3690a3b9
Trace Ohrt <teohrt18@gmail.com>
2018-09-26 14:29:48 -0500

    Tested. Submission ready.

--- IA02/ia02/logging.py
+++ IA02/ia02/logging.py
@@ -1,21 +1,13 @@
 """
-Trace Ohrt
-9/26/2018
-MIS407
+Helper module to log info about function calls
+Trace Ohrt 9/26/2018 MIS407
 """
-import os
-import sys
 from datetime import datetime
 
-"""
-Only allows the module to be used when it's imported
-"""
 if (__name__ == '__main__'):
     print("Sorry, not for human consumption.")
     quit()
 
-"""
-Logs datetime, function's module, name and message
-"""
 def log_function(f,  message):
+    """Logs datetime, function's module and name followed by a message"""
     print('{} {} {}: {}'.format(datetime.now().isoformat(sep=' '), f.__module__, f.__name__, message))
--- IA02/ia02_tester.py
+++ IA02/ia02_tester.py
@@ -1,9 +1,22 @@
+""" Test the logging functionality. """
 from ia02 import logging
+import csv
+import sys
 
-def hello():
-    print("Hello, world!")
+
+def read_csv(filename):
+    with open(filename, 'r', newline='') as reader:
+        csvReader = csv.reader(reader)
+        headers = next(csvReader)
+        rows = list(csvReader)
+        return headers, rows
 
 
-logging.log_function(hello, "Calling hello")
-hello()
-logging.log_function(hello, "Finished hello")
+for fn in sys.argv[1:]:
+    logging.log_function(read_csv, "Reading {}".format(fn))
+    headings, rows = read_csv(fn)
+    logging.log_function(read_csv, "Read {} rows from {}".format(len(rows), fn))
+
+print("Help on using ia02.logging:\n{}".format(logging.__doc__))
+print("Help on using ia02.logging log_function:\n{}".
+      format(logging.log_function.__doc__))
