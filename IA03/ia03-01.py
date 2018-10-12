e34d7a384d1b5a52e3047b01e37a6574f3a0b994
Trace Ohrt <teohrt18@gmail.com>
2018-10-12 11:43:03 -0500

    Iowa Liquor Sales dataset

---
+++ IA03/ia03-01.py
@@ -0,0 +1,28 @@
+import csv
+from collections import defaultdict
+
+fileName = "Iowa_Liquor_Sales-2017-partial.csv"
+
+total = 0
+table = defaultdict(lambda: 0)
+
+# Store individual values for countys in a dictionary
+with open(fileName) as csv_file :
+    reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in reader :
+        if line_count != 0:
+            # County is the dictionary key, liters is the value being added
+            k = row[8]
+            if k != "" :
+                table[(k[0].upper() + k[1:len(k)].lower())] += float(row[22])
+            total += float(row[22])
+            line_count += 1
+        line_count += 1
+
+count = 1
+for k, v in sorted(table.items()) :
+    print("{0:3}. {1:15} {2:>10} liters".format(count, k, round(v, 2)))
+    count += 1
+
+print(f"Total: {round(total, 2)} liters")
