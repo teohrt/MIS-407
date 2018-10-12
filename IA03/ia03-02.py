d0c7b9e8f85708733b1ebc8b2bccbabb774bfd73
Trace Ohrt <teohrt18@gmail.com>
2018-10-12 14:36:32 -0500

    Part two: Top 5

---
+++ IA03/ia03-02.py
@@ -0,0 +1,29 @@
+import csv
+import operator
+from collections import defaultdict
+
+fileName = "Iowa_Liquor_Sales-2017-partial.csv"
+
+total = 0
+table = defaultdict(lambda: 0)
+
+# Store individual values for countys in a dictionary
+with open(fileName) as csv_file :
+    reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in reader :
+        if line_count != 0:
+            # County is the dictionary key, liters is the value being added
+            k = row[8]
+            if k != "" :
+                table[k.title()] += float(row[22])
+            total += float(row[22])
+        line_count += 1
+
+print("Top 5 counties alcohol sales by volume:")
+
+count = 1
+for k, v in sorted(table.items(), key=operator.itemgetter(1), reverse=True) :
+    if count < 6 : 
+        print("{0:3}. {1:15} {2:>10} liters".format(count, k, round(v, 2)))
+    count += 1
